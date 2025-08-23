1cbb8474cc875bb5560f7ca37df68dd29970476b
Victor Avila <victoravilamorales95@gmail.com>
2025-08-22 19:45:41 -0700

    feat(app-sidebar): ✨ replace CommandIcon with logo image in sidebar

--- src/app/(authenticated)/_components/app-sidebar.tsx
+++ src/app/(authenticated)/_components/app-sidebar.tsx
@@ -1,4 +1,4 @@
-import { CommandIcon } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import {
   Sidebar,
@@ -19,11 +19,14 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
               <Link href="/">
-                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                  <CommandIcon className="size-4" />
-                </div>
+                <Image
+                  src="/images/docufai-logo-sm.png"
+                  alt="Docuf.ai Logo"
+                  width={32}
+                  height={32}
+                />
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">Docuf.ai</span>
+                  <span className="truncate font-medium">docuf.ai</span>
                   <span className="truncate text-xs">pdf solutions</span>
                 </div>
               </Link>
