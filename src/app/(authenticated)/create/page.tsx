18aba01fdbc8c7414f1e20e15a2572ecc4d8af78
Victor Avila <victoravilamorales95@gmail.com>
2025-08-23 22:21:37 -0700

    chore(create-page): 🔥 remove CreatePage component and its dependencies

--- src/app/(authenticated)/create/page.tsx
+++
@@ -1,5 +0,0 @@
-import { CreateDocumentContainer } from "~/features/document/components/create-document-container";
-
-export default function CreatePage() {
-  return <CreateDocumentContainer />;
-}
