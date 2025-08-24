e3a7df7b2691ee549907a7d7ee1f4d232a0c604d
Victor Avila <victoravilamorales95@gmail.com>
2025-08-24 13:03:25 -0700

    feat(create-page): ✨ add `CreatePage` component to handle document creation

---
+++ src/app/(authenticated)/create/page.tsx
@@ -0,0 +1,5 @@
+import { CreateDocumentContainer } from "~/features/document/components/create-document-container";
+
+export default function CreatePage() {
+  return <CreateDocumentContainer />;
+}
